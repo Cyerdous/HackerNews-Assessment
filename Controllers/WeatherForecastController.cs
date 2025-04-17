2cf4caf3874848edcc691a93c97bf7559e986e75
Cyer <cyerdous@gmail.com>
2025-04-16 21:01:53 -0400

    Change app proxy settings

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@
 namespace HackerNews_Assessment.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/[controller]")]
 public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
